f8645a00f8c02b986b3242cd160accfd0f90b04b
Jacob <36573259+Jacob10383@users.noreply.github.com>
2023-04-13 14:40:54 -0700

    Create Riverside Page (#1)
    * Create JacobToomeyPage.js
    
    * Add Page to route and rename page to riverside
    
    * Add to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
+
+import RiversidePage from "main/pages/RiversidePage";
+
 import IrvinePage from "main/pages/IrvinePage";
+
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -14,10 +18,14 @@ function App() {
     <BrowserRouter basename="/team00-s23-6pm-2">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
+        <Route exact path="/towns/" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
-        <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
+
+        <Route exact path="/towns/Riverside" element={<RiversidePage />} />
+
         <Route exact path="/towns/Irvine" element={<IrvinePage />} />
+        <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
+
       </Routes>
     </BrowserRouter>
   );
