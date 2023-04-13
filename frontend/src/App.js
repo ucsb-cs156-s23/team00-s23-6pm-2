381239455fbc010f16ee5619835e212ed58acfe8
Anand Giduthuri <agiduthuri@ucsb.edu>
2023-04-13 14:37:32 -0700

    ag - Added route in App.js for Sacramento

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
 import IrvinePage from "main/pages/IrvinePage";
+import SacramentoPage from "main/pages/SacramentoPage";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
         <Route exact path="/towns/Irvine" element={<IrvinePage />} />
+        <Route exact path="/towns/Sacramento" element={<SacramentoPage />} />
       </Routes>
     </BrowserRouter>
   );
