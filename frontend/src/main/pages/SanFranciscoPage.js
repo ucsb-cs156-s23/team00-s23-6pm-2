a2b5b72f0ce1da09ff8f830d85c8a530f68f3d58
FastTrackCoder <106446215+FastTrackCoder@users.noreply.github.com>
2023-04-13 14:34:51 -0700

    JG - add page for San Francisco

---
+++ frontend/src/main/pages/SanFranciscoPage.js
@@ -0,0 +1,19 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanFranciscoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Francisco, CA</h1>
+        <p>
+         San Francisco, California is a densely populated town in Northern California.
+	 It is surrounded by a large body of water which is why the 
+	 city has an abudnance of bridges and docks.  It is known for its significance 
+	 in the technology industry.
+        </p>
+        <p>Added by Jose G.</p>
+      </div>
+    </BasicLayout>
+  )
+}
+
