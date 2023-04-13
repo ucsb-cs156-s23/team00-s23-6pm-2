5b405046c01369cf5df11e742433ef018857def6
FastTrackCoder <106446215+FastTrackCoder@users.noreply.github.com>
2023-04-13 14:55:54 -0700

    JG - formatted City name in bold

--- frontend/src/main/pages/SanFranciscoPage.js
+++ frontend/src/main/pages/SanFranciscoPage.js
@@ -6,7 +6,7 @@ export default function SanFranciscoPage() {
       <div className="pt-2">
         <h1>San Francisco, CA</h1>
         <p>
-         San Francisco, California is a densely populated town in Northern California.
+         <b>San Francisco, California</b> is a densely populated town in Northern California.
 	 It is surrounded by a large body of water which is why the 
 	 city has an abudnance of bridges and docks.  It is known for its significance 
 	 in the technology industry.
