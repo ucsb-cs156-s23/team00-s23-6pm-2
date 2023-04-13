fe87adb0d4c97b5dd8189e04138b9cee3960cc70
Rylan Pow <rylan@ucsb.edu>
2023-04-13 14:02:00 -0700

    rp - add page for Mountain View

---
+++ frontend/src/main/pages/MountainViewPage.js
@@ -0,0 +1,18 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MountainViewPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Irvine, CA</h1>
+        <p>
+          <b>Mountain View, California</b> is a city in Santa Clara County, California.
+          Despite the ubiquitous greenery and tranquil suburban environment, Mountain View 
+          is a veritable juggernaught of technological innovation located in the heart of 
+          greater Silicon Valley.
+        </p>
+        <p>Added by Rylan P.</p>
+      </div>
+    </BasicLayout>
+  )
+}
